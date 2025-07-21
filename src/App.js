2a4a6a321bb899641f54e723cc685b1fae99a6ad
AngelaM288 <angelamartinezp@gmail.com>
2025-07-20 18:20:14 -0600

    Termine el diseño de peliculas en Django

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
-//scr/App.js
-import React from "react";
 import './App.css';
-import Login from "./Login";
-
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import MovieGallery from './MovieGallery';
+import Login from './Login';
 
 function App() {
+  const [token, setToken] = useState(null);
+
   return (
     <div className="App">
-
-      <Login />
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
+      {!token ? (
+        <Login setToken={setToken} />
+      ) : (
+        <MovieGallery token={token} />
+      )}
     </div>
   );
 }
--- src/Login.js
+++ src/Login.js
@@ -1,71 +1,69 @@
-//scr/Login.js
-import React, {useState} from "react";  
-import axios from "axios";
+// src/Login.js
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const Login = () => {
-  const [isRegistering, setIsRegistering] = useState(false);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [message, setMessage] = useState("");
+const Login = ({ setToken }) => {
+    const [isRegistering, setIsRegistering] = useState(false);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const url = isRegistering 
-        ? 'http://localhost:8000/api/register/' // endpoint para registro
-        : 'http://localhost:8000/api/login/';   // endpoint para login
-    
-    try {
-      const response = await axios.post(url, {
-        username, 
-        password 
-    });
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const url = isRegistering 
+            ? 'http://localhost:8000/api/register/'
+            : 'http://localhost:8000/api/login/';
 
-    if (isRegistering) {
-        setMessage('Registro exitoso. Ahora puedes iniciar sesión');
-    } else {
-        setMessage('Inicio de sesión exitoso');
-        console.log('Token recibido:', response.data);
-       } 
-    } catch (error) {
-      setMessage('Hubo un error. Verrifica tus datos.');
-    }
-  };
+        try {
+            const response = await axios.post(url, { 
+                username, 
+                password 
+            });
 
-  return (
-    <div>
-      <h1 className="cecyFlix-logo">CECYFLIX</h1>
-        <h2>{isRegistering ? 'Crear cuenta' : "Iniciar de Sesión"}</h2>
-        <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                placeholder="Usuario"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-            />
-            <input
-                type="password"
-                placeholder="Contraseña"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-            />
-            <button type="submit">
-                {isRegistering ? 'Registrarse' : 'Registrarse'}
-            </button>
-        </form>
-        <p>{message}</p>
-        <p>
-            {isRegistering ? '¿Ya tienes una cuenta? ' : '¿No tienes cuenta? '}{' '}
-            <span
-                onClick={() => setIsRegistering(!isRegistering)}
-                style={{ cursor: 'pointer', color: '#e50914' }}
-            >
-                {isRegistering ? 'Iniciar sesión' : 'Registrate'}
-            </span>
-        </p>
-    </div>
-  );
+            if (isRegistering) {
+                setMessage('Registro exitoso. Ahora puedes iniciar sesión.');
+            } else {
+                setMessage('Inicio de sesión exitoso.');
+                setToken(response.data.access);
+            }
+        } catch (error) {
+            setMessage('Hubo un error verifica tus datos.');
+        }
+    };
+
+    return (
+        <div>
+            <h1 className="cecyflix-logo">CECYFLIX</h1>
+            <h2>{isRegistering ? 'Crear cuenta' : 'Iniciar sesión'}</h2>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    placeholder="Usuario" 
+                    value={username} 
+                    onChange={(e) => setUsername(e.target.value)} 
+                />
+                <input 
+                    type="password" 
+                    placeholder="Contraseña" 
+                    value={password} 
+                    onChange={(e) => setPassword(e.target.value)} 
+                />
+                <button type="submit">
+                    {isRegistering ? 'Registrarse' : 'Ingresar'}
+                </button>
+            </form>
+            <p>{message}</p>
+            <p>
+                {isRegistering ? '¿Ya tienes una cuenta?' : '¿No tienes cuenta?'}{' '}
+                <span 
+                    onClick={() => setIsRegistering(!isRegistering)}
+                    style={{ cursor: 'pointer', color: '#e50914' }}
+                >
+                    {isRegistering ? 'Inicia sesión' : 'Regístrate'}
+                </span>
+            </p>
+        </div>
+    );
 };
+
 export default Login;
-
-
